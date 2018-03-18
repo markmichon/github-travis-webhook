ba75183542ad3237c1956ddd14890a748b680244
markmichon <hello@markmichon.com>
2018-03-18 14:35:12 -0400

    Fix pre-sync bug

--- server.js
+++ server.js
@@ -1,80 +1,99 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var axios = require('axios');
-var app = express();
- 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
- 
+var express = require("express")
+var bodyParser = require("body-parser")
+var axios = require("axios")
+var app = express()
 
-app.post('/activate/:token', activate)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.post("/activate/:token", activate)
 
+app.listen(3000, () => console.log("listening on port 3000!"))
 
 function activate(req, res) {
   const token = req.params.token
-  if (req.headers['x-github-event'] && req.headers['x-github-event'] === 'repository' && req.body.action && req.body.action === 'created') {
+  if (
+    req.headers["x-github-event"] &&
+    req.headers["x-github-event"] === "repository" &&
+    req.body.action &&
+    req.body.action === "created"
+  ) {
+    // Sending initial status 200 back to avoid webhook timeout
+    res.status("200").send("Activation attempt in progress")
     const reponame = encodeURIComponent(req.body.repository.full_name)
-    setTimeout( () => {
-      getUserId(token)
-      
-      .then( id => updateSyncStatus(token, id)
-            
-            .then(() => {
-        activateRepository(token, reponame)
-          
-          .then(data => {
-          res.status('200').send('Repo updated')
+    getUserId(token)
+      .then(id =>
+        updateSyncStatus(token, id).then(data => {
+          activateRepository(token, reponame)
         })
-      })).catch( err => res.status('500').send('error'))
-    }, 10000)
-      
-    } else {
-      res.status('200').send('Non-repository event; ignoring')
-    }
-  
-
-  
+      )
+      .catch(err => console.log("Error occurred", err))
+  } else {
+    res.status("200").send("Non-repository event; ignoring")
+  }
 }
 
 function getUserId(token) {
   return axios({
-    method: 'get',
+    method: "get",
     url: `https://api.travis-ci.com/user`,
     headers: {
-    Authorization: `token ${token}`,
-     'Travis-API-Version': '3'
+      Authorization: `token ${token}`,
+      "Travis-API-Version": "3"
     }
   })
-  .catch(err => console.log('error getting user id'))
-  .then(res => {
-    return res.data.id
-  })
+    .catch(err => console.log("error getting user id"))
+    .then(res => {
+      return res.data.id
+    })
 }
 
 function updateSyncStatus(token, userId) {
-    return axios({
-      method: 'post',
-      headers: {
-    Authorization: `token ${token}`,
-     'Travis-API-Version': '3'
-    },
-      url: `https://api.travis-ci.com/user/${userId}/sync`
-    }).catch(err => console.log('error syncing user: ', err))
-}
-
-function activateRepository(token, reponame) { 
-  return setTimeout( () => {
   return axios({
-    method: 'post',
-    url: `https://api.travis-ci.com/repo/${reponame}/activate`,
+    method: "post",
     headers: {
       Authorization: `token ${token}`,
-     'Travis-API-Version': '3' } 
+      "Travis-API-Version": "3"
+    },
+    url: `https://api.travis-ci.com/user/${userId}/sync`
+  })
+    .catch(err => console.log("error syncing user: ", err))
+    .then(res => res.data)
+}
+
+function isSyncing(token) {
+  console.log("checking sync status")
+  return axios({
+    method: "get",
+    url: `https://api.travis-ci.com/user`,
+    headers: {
+      Authorization: `token ${token}`,
+      "Travis-API-Version": "3"
+    }
+  })
+    .catch(err => console.log("error checking sync status"))
+    .then(res => {
+      console.log(res.data.is_syncing)
+      return res.data.is_syncing
+    })
+}
+
+async function activateRepository(token, reponame) {
+  const syncing = await isSyncing(token)
+  if (syncing) {
+    console.log("Sync in progress")
+    setTimeout(() => {
+      activateRepository(token, reponame)
+    }, 1000)
+  } else {
+    console.log("Sync complete, activating repo")
+    return axios({
+      method: "post",
+      url: `https://api.travis-ci.com/repo/${reponame}/activate`,
+      headers: {
+        Authorization: `token ${token}`,
+        "Travis-API-Version": "3"
+      }
+    }).catch(err => console.log("Error activating repo"))
   }
-  )
-  .catch(err => console.log('Error activating repo'))  
-  }, 3000)
-  
 }
